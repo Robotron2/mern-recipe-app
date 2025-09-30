b25c7f85acdc13beca63ff10efbcab1638a1af24
Theophilus <theophilusadesola002@gmail.com>
2025-09-30 16:12:10 +0100

    Backend: User routes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ app.use( cors() )
 app.use( express.json() )
 
 const recipeRoute = require( "./routes/recipe" )
+const userRoute = require( "./routes/user" )
 
+app.use( "/users", userRoute )
 app.use( "/recipe", recipeRoute )
 
 app.listen( PORT, ( err ) => {
