45583cd4dcf4a1de49d00778519e406cffcd4717
Theophilus <theophilusadesola002@gmail.com>
2025-09-30 16:04:19 +0100

    Backend: CORS fixed

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ require( "dotenv" ).config()
 const express = require( "express" )
 const app = express()
 const connectDB = require( "./config/db" )
+const cors = require( "cors" )
 
 const PORT = process.env.PORT || 8000
 connectDB()
+app.use( cors() )
 app.use( express.json() )
 
 const recipeRoute = require( "./routes/recipe" )
